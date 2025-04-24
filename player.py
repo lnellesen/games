c90a2aa8635f92b9f7ed8093ab85b1fc1df9e3d8
Leonie Nellesen <leonie.nellesen@gmail.com>
2025-04-24 13:00:25 +0200

    added function that checks if on player is on top pf another player. If thats the case the new player sits on top of older player and does not fall through to the gorund

--- player.py
+++ player.py
@@ -42,7 +42,32 @@ class Player:
             self.falling = True
         if not self.on_ground and self.falling:
             self.y += self.fall_velocity * self.game.delta_time
+
+            # Check for collision with other players
+            for other in self.game.players:
+                if other is self:
+                    continue
+                if self._is_falling_on_top_of(other):
+                    self.y = other.y - self.height
+                    self.on_ground = True
+                    self.falling = False
+                    return
+
+            # Check collision with the ground
             if self.y >= self.game.screen_height - self.height:
                 self.y = self.game.screen_height - self.height
                 self.on_ground = True
                 self.falling = False
+
+
+    def _is_falling_on_top_of(self, other):
+        # Check if this player is falling onto another player
+        horizontally_aligned = (
+            self.x + self.width > other.x and
+            self.x < other.x + other.width
+        )
+        vertically_touching = (
+            self.y + self.height >= other.y and
+            self.y + self.height - self.fall_velocity * self.game.delta_time < other.y
+        )
+        return horizontally_aligned and vertically_touching
