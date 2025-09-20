8c97920e87bf95d8ba0ed99e9f8030a73b602d77
Leonie Nellesen <leonie.nellesen@gmail.com>
2025-09-20 20:19:36 +0200

    testing stop_falling method as an example

---
+++ tests/test_player.py
@@ -0,0 +1,19 @@
+"""
+testing stop falling as an example test (jon hat gesagt das reicht :))
+"""
+from main import Game
+from player import Player
+
+
+def test_stop_falling():
+
+    test_game = Game()
+    player = Player(test_game, 1)
+
+    assert not player.on_ground
+    assert not player._falling
+
+    player._stop_falling()
+
+    assert player.on_ground
+    assert not player._falling, 'after calling stop falling _falling should be False'
